74cfe31cfebf21d233a2444682b23521275327e8
Roman Komarov <kizmarh@ya.ru>
2024-10-22 21:50:30 +0200

    Maybe fix Safari?

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import './prism.js';
+import initPrism from './prism.js';
 import initPjax from './pjax.js';
 import search from './search.js';
 import initBlockLinks from './blockLinks';
@@ -10,8 +10,17 @@ window.mySearch = search;
 const queue = new IdleQueue({ ensureTasksRun: true });
 const lazyQueue = new IdleQueue();
 
-initPjax(queue, lazyQueue);
+lazyQueue.pushTask(initWebComponents);
+window.Prism = window.Prism || {};
+Prism.manual = true;
+Prism.disableWorkerMessageHandler = true;
+lazyQueue.pushTask(initPrism);
+lazyQueue.pushTask(() => initBlockLinks('.ArticleList-Item'));
+lazyQueue.pushTask(() => initPjax(queue, lazyQueue));
 
-initBlockLinks('.ArticleList-Item');
-
-initWebComponents();
+// Hey, Safari, maybe only load Prism after 10 seconds???
+if (!('requestIdleCallback' in window) && document.location.pathname === '/tree-counting-and-random/') {
+  console.log('Ooops, running Prism freezes the page on this URL. I am investigating, but for now Prism is disabled here.')
+} else {
+  lazyQueue.pushTask(() => Prism.highlightAll());
+}
